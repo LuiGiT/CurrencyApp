69a74b2c9496885548153d3e9f5f286fbd0fa33c
Luigi Antoine Terrero <luigiterrero@gmail.com>
2021-04-27 11:36:35 -0400

    Added purchase features

---
+++ CurrencyApp/Controllers/CurrencyPurchaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApp.Controllers
+{
+    [Route("api/purchase")]
+    [ApiController]
+    public class CurrencyPurchaseController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> purchaseCurrency(string userid, double amount, string iso)
+        {
+            CurrencyPurchase cp = new CurrencyPurchase();
+            var purchase = await cp.getCurrencyPurchase(userid, amount, iso);
+
+            return Ok(purchase);
+        }
+    }
+}
---
+++ CurrencyApp/CurrencyPurchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApp
+{
+    public class CurrencyPurchase
+    {
+        public async Task<string> getCurrencyPurchase(string userId, double amount, string iso)
+        {
+            CurrencyExchange ce = new CurrencyExchange();
+            var currRate = await ce.calculateExchange(iso);
+            if (currRate.success)
+                return (amount / double.Parse(currRate.response)).ToString();
+            else
+                return currRate.errorMessage;
+        }
+    }
+}
