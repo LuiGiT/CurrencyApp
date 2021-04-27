dba20d31454613b64c3796e478efc090858bd9e0
Luigi Antoine Terrero <luigiterrero@gmail.com>
2021-04-27 11:07:45 -0400

    adding models

---
+++ CurrencyApp/Models/CurrencyConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApp.Models
+{
+    public class CurrencyConfig
+    {
+        public string endpoint { get; set; }
+        public List<Currency> currencies { get; set; }
+    }
+}
---
+++ CurrencyApp/Models/ResponseBody.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApp.Models
+{
+    public class ResponseBody
+    {
+        public string response { get; set; }
+        public bool success { get; set; }
+        public string errorMessage { get; set; }
+    }
+}
